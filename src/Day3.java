2ccbe3dd790680c38282fe8d7fa3f24086656804
sabita <sabitakhadka091@gmail.com>
2024-09-11 15:26:04 +0545

    feat: Add examples of type conversion in Java (implicit and explicit)

--- src/Day3.java
+++ src/Day3.java
@@ -1,5 +1,20 @@
 public class Day3 {
     public static void main(String[] args) {
-        
+        float age1 = 2.1234567f; 
+        double age2 = 2.123456789101112;
+        System.out.println(age1);
+        System.out.println(age2);
+
+        int num = 10;
+        double num0 = num;  // Implicit conversion from int to double
+       System.out.println(num0);  // Output will be 10.0
+    
+       double num1 = 9.78;
+       int num2 = (int) num1;  // Explicit conversion from double to int
+       System.out.println(num2);  // Output will be 9 (fraction part is lost)
+
+    //    int num = (int) true;  // Compilation error: incompatible types
+  
+
     }
 }
