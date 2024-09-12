0d66912e52683ca56c41873727afdab8608e0d88
sabita <sabitakhadka091@gmail.com>
2024-09-12 16:04:03 +0545

    feat: Added examples of conditional statements (if, if-else, else-if, switch) in Java

--- src/Day4/Day4.java
+++ src/Day4/Day4.java
@@ -2,6 +2,45 @@ package Day4;
 
 public class Day4 {
     public static void main(String[] args) {
-        
+        // if statement
+        int age = 18;
+     if (age >= 18) {
+    System.out.println("You are an adult.");
+   }
+
+//    if-else statement
+ if (age >= 18) {
+    System.out.println("You are an adult.");
+} else {
+    System.out.println("You are a minor.");
+}
+// else-if 
+int marks = 85;
+if (marks >= 90) {
+    System.out.println("Grade A");
+} else if (marks >= 75) {
+    System.out.println("Grade B");
+} else if (marks >= 60) {
+    System.out.println("Grade C");
+} else {
+    System.out.println("Fail");
+}
+
+// switch statement
+int day = 0;
+switch (day) {
+    case 1:
+        System.out.println("Monday");
+        break;
+    case 2:
+        System.out.println("Tuesday");
+        break;
+    case 3:
+        System.out.println("Wednesday");
+        break;
+    default:
+        System.out.println("Invalid day");
+}
+  
     }
 }
