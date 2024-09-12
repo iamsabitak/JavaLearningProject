242b92683d581d89122484a6c0f917b264d75605
sabita <sabitakhadka091@gmail.com>
2024-09-12 16:28:34 +0545

    feat:Add solutions for even/odd check, number comparison, and voting eligibility in Java.

--- src/Day4/Day4.java
+++ src/Day4/Day4.java
@@ -1,5 +1,7 @@
 package Day4;
 
+import java.util.Scanner;
+
 public class Day4 {
     public static void main(String[] args) {
         // if statement
@@ -9,18 +11,18 @@ public class Day4 {
    }
 
 //    if-else statement
- if (age >= 18) {
-    System.out.println("You are an adult.");
+ if (age <= 17) {
+    System.out.println("You are an minor.");
 } else {
-    System.out.println("You are a minor.");
+    System.out.println("You are a adult.");
 }
 // else-if 
 int marks = 85;
 if (marks >= 90) {
     System.out.println("Grade A");
-} else if (marks >= 75) {
+} else if (marks >= 80) {
     System.out.println("Grade B");
-} else if (marks >= 60) {
+} else if (marks >= 70) {
     System.out.println("Grade C");
 } else {
     System.out.println("Fail");
@@ -41,6 +43,51 @@ switch (day) {
     default:
         System.out.println("Invalid day");
 }
+
+    // Problem no 1 Write a program that checks if a number is even or odd.
+ Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is even.");
+        } else {
+            System.out.println(number + " is odd.");
+        }
+       
+      //Problem no 2 Program to Compare Two Numbers
+        Scanner scanner1 = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int num1 = scanner1.nextInt();
+        
+        System.out.print("Enter the second number: ");
+        int num2 = scanner1.nextInt();
+
+        if (num1 == num2) {
+            System.out.println(num1 + " and " + num2 + " are equal.");
+        } else if (num1 > num2) {
+            System.out.println(num1 + " is greater than " + num2 + ".");
+        } else {
+            System.out.println(num1 + " is less than " + num2 + ".");
+        }
+
+          //Problem 3 Java Program to Check Voting Eligibility
+        Scanner scanner2 = new Scanner(System.in);
+        System.out.print("Enter your age: ");
+        int agee = scanner2.nextInt();
+        if (agee >= 18) {
+            System.out.println("You are eligible to vote.");
+        } else {
+            System.out.println("You are not eligible to vote.");
+        }
+
+        scanner.close();
+
+        scanner1.close();
+
+        scanner2.close();
   
     }
 }
