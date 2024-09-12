a8b695ec5747a3ef59f33be6ce6deb6373384c19
sabita <sabitakhadka091@gmail.com>
2024-09-12 17:05:55 +0545

    feat: Add basic calculator with operator validation and division by zero check.

--- src/Day4/BasicCalculator.java
+++ src/Day4/BasicCalculator.java
@@ -1,7 +1,38 @@
 package Day4;
 
+import java.util.Scanner;
+
 public class BasicCalculator {
     public static void main(String[] args) {
-        
+        // Problem 8: Implement a basic calculator using the switch statement.
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter First Number :");
+        double numb1 = scanner.nextDouble();
+        System.out.println("Enter Second Number :");
+        double numb2 = scanner.nextDouble();
+
+        System.out.println("Choose an operation: +, -, *, /");
+        char operator = scanner.next().charAt(0);
+
+        switch (operator) {
+            case '+':
+                System.out.println(numb1 + numb2);
+                break;
+
+            case '/':
+                System.out.println(numb1 / numb2);
+                break;
+            case '-':
+                System.out.println(numb1 - numb2);
+                break;
+            case '*':
+                System.out.println(numb1 * numb2);
+                break;
+            default:
+                System.out.println("Invalid operator. Please choose +, -, *, or /.");
+        }
+
+        scanner.close();
     }
 }
