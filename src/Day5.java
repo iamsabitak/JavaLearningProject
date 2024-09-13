10be949126ba7606ba9dc31b0983f0266242d898
sabita <sabitakhadka091@gmail.com>
2024-09-13 15:30:09 +0545

    feat: implement leap year checker using ternary operator with complete leap year conditions

--- src/Day5.java
+++ src/Day5.java
@@ -21,8 +21,14 @@ public class Day5 {
             double result = a - b;
             System.out.println("Subtraction : " + result);
         } else if (opertaor == '/') {
-            double result = a / b;
-            System.out.println("Division : " + result);
+            // nested
+            if (b != 0) {
+                double result = a / b;
+                System.out.println("Division : " + result);
+            } else {
+                System.out.println("cannot divided by zero");
+            }
+
         } else if (opertaor == '*') {
             double result = a * b;
             System.out.println("Multiplication : " + result);
@@ -32,34 +38,52 @@ public class Day5 {
 
         // using Switch
         // switch (opertaor) {
-        //     case '+': {
-        //         double result = a + b;
-        //         System.out.println("Addition : " + result);
-        //         break;
-        //     }
-        //     case '-': {
-        //         double result = a - b;
-        //         System.out.println("Subtraction : " + result);
-        //         break;
-        //     }
-        //     case '*': {
-        //         double result = a * b;
-        //         System.out.println("Multiplication : " + result);
-        //         break;
-        //     }
-        //     case '/': {
-        //         double result = a / b;
-        //         System.out.println("Division : " + result);
-        //         break;
-        //     }
-        //     default:
-        //         System.out.println("Invalid operator");
+        // case '+': {
+        // double result = a + b;
+        // System.out.println("Addition : " + result);
+        // break;
+        // }
+        // case '-': {
+        // double result = a - b;
+        // System.out.println("Subtraction : " + result);
+        // break;
+        // }
+        // case '*': {
+        // double result = a * b;
+        // System.out.println("Multiplication : " + result);
+        // break;
+        // }
+        // case '/': {
+        // double result = a / b;
+        // System.out.println("Division : " + result);
+        // break;
+        // }
+        // default:
+        // System.out.println("Invalid operator");
 
         // }
 
-      
+        // Ternery opertor
 
+        int number = 2;
 
+        String result = (number % 2 == 0) ? "even" : "odd";
+
+        System.out.println(result);
+
+        // Write a program to check whether a given year is a leap year or not using the
+        // ternary operator.
+
+        Scanner yr = new Scanner(System.in);
+
+        System.out.println("Enter the year : ");
+        int year = yr.nextInt();
+        String resultY = (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) ? "Leap Year" : "Not a Leap Year";
+
+        System.out.println(resultY);
+
+        
+        yr.close();
         sc.close();
     }
 }
