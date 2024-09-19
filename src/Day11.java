6f07828db27ac1b40132adb68452af2532b341b7
sabita <sabitakhadka091@gmail.com>
2024-09-19 14:07:20 +0545

    feat: created basic java method to greet

---
+++ src/Day11.java
@@ -0,0 +1,9 @@
+public class Day11 {
+    public static void main(String[] args) {
+printGreeting("Sabita");
+    }
+
+    public static void printGreeting(String name) {
+        System.out.println("greet" + name);
+    }
+}
