e3e34e09b29003a63f13e79f33b3a0fef15bfb31
sabita <sabitakhadka091@gmail.com>
2024-09-26 13:33:11 +0545

    Refactor: Rock-Paper-Scissors game for improved input validation and concise game logic.

--- src/Day18/RockPaperScissor.java
+++ src/Day18/RockPaperScissor.java
@@ -10,23 +10,41 @@ public class RockPaperScissor {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter 0 for Rock, 1 for Paper, 2 for Scissor: ");
         int user = sc.nextInt();
-        int randomInt = (int) (Math.random() * 3);
-        if (user == 0 && randomInt == 1) {
-            System.out.println("Computer Win : " + randomInt + " You Lose : " + user);
-        } else if (user == 1 && randomInt == 0) {
-            System.out.println("You Win : " + user + " Computer Lose : " + randomInt);
-        } else if (user == 0 && randomInt == 2) {
-            System.out.println("You Win : " + user + " Computer Lose : " + randomInt);
-        } else if (user == 2 && randomInt == 0) {
-            System.out.println("Computer Win : " + randomInt + " You Lose : " + user);
-        } else if (user == 1 && randomInt == 2) {
-            System.out.println("Computer Win : " + randomInt + " You Lose : " + user);
-        } else if (user == 2 && randomInt == 1) {
-            System.out.println("You Win : " + user + " Computer Lose : " + randomInt);
-        } else if (user == randomInt) {
-            System.out.println("You choose : " + user + " Computer chose : " + randomInt + "Its a Tie.");
+        // int randomInt = (int) (Math.random() * 3);
+        // if (user == 0 && randomInt == 1) {
+        // System.out.println("Computer Win : " + randomInt + " You Lose : " + user);
+        // } else if (user == 1 && randomInt == 0) {
+        // System.out.println("You Win : " + user + " Computer Lose : " + randomInt);
+        // } else if (user == 0 && randomInt == 2) {
+        // System.out.println("You Win : " + user + " Computer Lose : " + randomInt);
+        // } else if (user == 2 && randomInt == 0) {
+        // System.out.println("Computer Win : " + randomInt + " You Lose : " + user);
+        // } else if (user == 1 && randomInt == 2) {
+        // System.out.println("Computer Win : " + randomInt + " You Lose : " + user);
+        // } else if (user == 2 && randomInt == 1) {
+        // System.out.println("You Win : " + user + " Computer Lose : " + randomInt);
+        // } else if (user == randomInt) {
+        // System.out.println("You choose : " + user + " Computer chose : " + randomInt
+        // + "Its a Tie.");
+        // } else {
+        // System.out.println("Invalid input! Please enter 0 for Rock, 1 for Paper, or 2
+        // for Scissor.");
+        // }
+
+        if (user < 0 || user > 2) {
+            System.out.println("Invalid input! Please enter 0 for Rock, 1 for Paper, or 2 for Scissor.");
         } else {
-            System.out.println("Invalid number enter 0, 1, 2");
+            int randomInt = (int) (Math.random() * 3);
+
+            if (user == randomInt) {
+                System.out.println("It's a Tie! You both chose " + user);
+            } else if ((user == 0 && randomInt == 2) ||
+                    (user == 1 && randomInt == 0) ||
+                    (user == 2 && randomInt == 1)) {
+                System.out.println("You Win! You chose " + user + " and the computer chose " + randomInt);
+            } else {
+                System.out.println("You Lose! You chose " + user + " and the computer chose " + randomInt);
+            }
         }
 
         sc.close();
