770c3c6d159440013a917bf874d7dc56bc5cd126
sabita <sabitakhadka091@gmail.com>
2024-10-03 09:03:48 +0545

    feat: Implemented Lambda Expressions for String Concatenation and Calculator Functional Interfaces

---
+++ src/Day24/LambdaStringExample.java
@@ -0,0 +1,24 @@
+package Day24;
+
+@FunctionalInterface
+interface StringConcatenator {
+    String concatenate(String str1, String str2);
+}
+interface Calculator {
+    int add(int a, int b);
+}
+
+public class LambdaStringExample {
+    public static void main(String[] args) {
+        // Lambda expression implementing the concatenate method
+        StringConcatenator concat = (str1, str2) -> str1 + " " + str2;
+        
+        // Test the lambda expression
+        String result = concat.concatenate("Hello", "World");
+        System.out.println(result);  // Output: Hello World
+
+        Calculator calculator = (a, b) -> a + b;
+        
+        System.out.println("Sum: " + calculator.add(5, 3));
+    }
+}
